e37ae657d889e84be9b6aa21528a05e383d4c0d8
Andy Uzick <andy@uzick.com>
2017-07-31 17:44:15 -0500

    Fixed bug where the app hung when the blob being tailed is deleted from the container.

---
+++ BlobTail/Exceptions/BlobNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobTail.Exceptions
+{
+    public class BlobNotFoundException : Exception
+    {
+        public BlobNotFoundException()
+        {
+        }
+
+        public BlobNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public BlobNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
--- BlobTail/Form1.cs
+++ BlobTail/Form1.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
+using BlobTail.Exceptions;
 using BlobTail.Properties;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
@@ -17,24 +18,29 @@ namespace BlobTail
 
         public static class TailStatus
         {
-            public static readonly int TextRead = 0;
-            public static readonly int TextReadToEnd = 1;
-            public static readonly int AtEnd = 2;
+            public static readonly int TextRead = 1;
+            public static readonly int TextReadToEnd = 2;
+            public static readonly int AtEnd = 3;
+
+            public static readonly int ErrorThreshold = 50;
+
+            public static readonly int UnknownError = 51;
+            public static readonly int BlobNotFoundError = 52;
         }
 
-        private BackgroundWorker _tailWorker;
-        private bool _resetTail = false;
+        private readonly BackgroundWorker _tailWorker;
+        private readonly object _resetLock = new object();
+        private bool _doReset = false;
         private CloudBlob _currentBlob;
 
         private CloudBlobClient _blobClient;
         private CloudBlobClient BlobClient => _blobClient ?? (_blobClient = StorageAccount.CreateCloudBlobClient());
 
         private CloudStorageAccount _storageAccount;
-        public CloudStorageAccount StorageAccount {
-            get
-            {
-                return _storageAccount;
-            }
+
+        public CloudStorageAccount StorageAccount
+        {
+            get { return _storageAccount; }
             set
             {
                 _storageAccount = value;
@@ -45,19 +51,27 @@ namespace BlobTail
         public Form1()
         {
             InitializeComponent();
+
+            _tailWorker = new BackgroundWorker {WorkerSupportsCancellation = false};
+            _tailWorker.DoWork += TailWorker_DoWork;
+            _tailWorker.ProgressChanged += TailWorker_ProgressChanged;
+            _tailWorker.WorkerReportsProgress = true;
+            var parameters = new object[] {};
+            _tailWorker.RunWorkerAsync(parameters);
+
             ConnectionString.Text = Settings.Default["StorageAccountConnectionString"].ToString();
             if (!string.IsNullOrWhiteSpace(ConnectionString.Text))
             {
                 StorageAccount = CloudStorageAccount.Parse(ConnectionString.Text);
                 EnumerateContainers();
             }
+
             ConnectButton.Enabled = true;
             StopButton.Enabled = false;
         }
 
         private void EnumerateContainers()
         {
-            LogText.Text = "";
             BlobList.Items.Clear();
             ContainerList.Items.Clear();
             ContainerList.Items.Add("Select...");
@@ -65,6 +79,7 @@ namespace BlobTail
             {
                 ContainerList.Items.Add(container.Name);
             }
+            ContainerList.SelectedIndex = 0;
         }
 
         private void EnumerateBlobs()
@@ -73,10 +88,10 @@ namespace BlobTail
             var container = GetSelectedContainer();
             if (container == null) return;
 
-            var blobs = container
-                .ListBlobs()
-                .OfType<CloudBlob>()
-                .OrderBy(b => b.Properties.LastModified);
+            var blobs = GetBlobs(container);
+            //.ListBlobs()
+            //.OfType<CloudBlob>()
+            //.OrderBy(b => b.Properties.LastModified);
 
             foreach (var blob in blobs)
             {
@@ -88,6 +103,7 @@ namespace BlobTail
                 BlobList.SelectedIndex = BlobList.Items.Count - 1;
             }
         }
+
 
         private CloudBlobContainer GetSelectedContainer()
         {
@@ -103,152 +119,136 @@ namespace BlobTail
             return container?.GetBlobReference(name);
         }
 
-        private bool RefreshBlobsInCurrentContainer()
+        private void RefreshBlobsInCurrentContainer()
         {
             BlobListLabel.ForeColor = Color.White;
             BlobListLabel.Refresh();
             var container = GetSelectedContainer();
-            if (container == null) return false;
-            var blobs = container.ListBlobs();
-            if (blobs.Count() > BlobList.Items.Count)
+            if (container == null) return;
+            var blobs = GetBlobs(container);
+            if (blobs.Count() != BlobList.Items.Count)
             {
-                //_tailWorker.CancelAsync();
-                _currentBlob = null;
-                _resetTail = true;
+                ResetTail();
                 EnumerateBlobs();
-                return true;
+                //StartTail();
+                return;
             }
             BlobListLabel.ForeColor = SystemColors.ControlText;
             BlobListLabel.Refresh();
-            return false;
+            return;
         }
 
-        private bool SetupConnection()
+        private void StartTail()
         {
-            try
-            {
-                StorageAccount = CloudStorageAccount.Parse(ConnectionString.Text);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                return false;
-            }
-            return true;
-        }
-
-        private void TailBlob()
-        {
-            LogText.Text = "";
             _currentBlob = GetSelectedBlob();
-            _resetTail = true;
-
             if (_currentBlob == null)
                 return;
 
-            if (_tailWorker == null)
-            {
-                _tailWorker = new BackgroundWorker {WorkerSupportsCancellation = false};
-                _tailWorker.DoWork += TailWorker_DoWork;
-                _tailWorker.ProgressChanged += TailWorker_ProgressChanged;
-                _tailWorker.WorkerReportsProgress = true;
-                //_tailWorker.WorkerSupportsCancellation = true;
-                //var parameters = new object[] { blob };
-                var parameters = new object[] { };
-                _tailWorker.RunWorkerAsync(parameters);
-            }
+            LogText.Text = "";
+            _doReset = true;
+
+            ConnectionString.Enabled = false;
             ConnectButton.Enabled = false;
             StopButton.Enabled = true;
         }
 
-        private void TailWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        private void ResetTail()
         {
-            if (e.ProgressPercentage == TailStatus.TextRead || e.ProgressPercentage == TailStatus.TextReadToEnd)
+            lock (_resetLock)
             {
-                LogText.AppendText(e.UserState.ToString());
-            }
-            else if (e.ProgressPercentage == TailStatus.AtEnd)
-            {
-                RefreshBlobsInCurrentContainer();
-            }
-        }
+                _currentBlob = null;
+                _doReset = true;
+                LogText.Text = "";
 
-        private void ConnectButton_Click(object sender, EventArgs e)
-        {
-            if (SetupConnection())
-            {
-                EnumerateContainers();
+                ConnectionString.Enabled = true;
+                ConnectButton.Enabled = true;
+                StopButton.Enabled = false;
             }
-        }
-
-        private void ContainerList_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            LogText.Text = "";
-            EnumerateBlobs();
-        }
-
-        private void BlobList_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            LogText.Text = "";
-            TailBlob();
         }
 
         private void TailWorker_DoWork(object sender, DoWorkEventArgs e)
         {
-            var bufferSize = 4096;
+            const int bufferSize = 4096;
             //var parameters = e.Argument as object[];
             //var blob = (CloudBlob) parameters[0];
-            var bytesReceived = (long)0;
+            var bytesReceived = (long) 0;
 
             do
             {
-                //if (_tailWorker.CancellationPending)
-                //{
-                //    e.Cancel = true;
-                //    return;
-                //}
+                if (_tailWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
 
                 while (_currentBlob == null)
                 {
                     Thread.Sleep(SleepInterval);
                 }
 
-                if (_resetTail)
+                if (_doReset)
                 {
-                    bytesReceived = 0;
-                    _resetTail = false;
+                    lock (_resetLock)
+                    {
+                        bytesReceived = 0;
+                        _doReset = false;
+                    }
                 }
 
-                _currentBlob.FetchAttributes();
-                var blobSize = _currentBlob.Properties.Length;
-                var bytesToDownload = (blobSize - bytesReceived <= bufferSize)
-                    ? (int)(_currentBlob.Properties.Length - bytesReceived)
-                    : bufferSize;
-
-                var bytesFetched = 0;
+                int status; 
                 var text = string.Empty;
-                var buffer = new byte[bytesToDownload];
-                if (bytesToDownload > 0)
-                {
-                    bytesFetched = _currentBlob.DownloadRangeToByteArray(buffer, 0, bytesReceived, bytesToDownload);
-                    bytesReceived += bytesFetched;
-                    text = (System.Text.Encoding.ASCII.GetString(buffer));
-                }
+                var bytesFetched = 0;
 
-                int status;
-                if (bytesFetched == 0)
-                    status = TailStatus.AtEnd;
-                else if (bytesFetched == bufferSize)
-                    status = TailStatus.TextRead;
-                else
-                    status = TailStatus.TextReadToEnd;
+                try
+                {
+                    if (!_currentBlob.Exists())
+                    {
+                        throw new BlobNotFoundException($"The blob \"{_currentBlob?.Name ?? "[unknown]"}\" does not exist.");
+                    }
+
+                    _currentBlob.FetchAttributes();
+                    var blobSize = _currentBlob.Properties.Length;
+                    var bytesToDownload = (blobSize - bytesReceived <= bufferSize)
+                        ? (int) (_currentBlob.Properties.Length - bytesReceived)
+                        : bufferSize;
+
+                    var buffer = new byte[bytesToDownload];
+                    if (bytesToDownload > 0)
+                    {
+                        bytesFetched = _currentBlob.DownloadRangeToByteArray(buffer, 0, bytesReceived, bytesToDownload);
+                        bytesReceived += bytesFetched;
+                        text = (System.Text.Encoding.ASCII.GetString(buffer));
+                    }
+
+                    if (bytesFetched == 0)
+                        status = TailStatus.AtEnd;
+                    else if (bytesFetched == bufferSize)
+                        status = TailStatus.TextRead;
+                    else
+                        status = TailStatus.TextReadToEnd;
+                }
+                catch (BlobNotFoundException blobNotFoundEx)
+                {
+                    _currentBlob = null;
+                    status = TailStatus.BlobNotFoundError;
+                    text = string.IsNullOrWhiteSpace(blobNotFoundEx.Message) 
+                        ? "The blob does not exist."
+                        : blobNotFoundEx.Message;
+
+                }
+                catch (Exception ex)
+                {
+                    _currentBlob = null;
+                    status = TailStatus.UnknownError;
+                    text = ex.Message;
+                }
 
                 _tailWorker.ReportProgress(status, text);
 
-                if (bytesFetched < bufferSize)
+                if (bytesFetched < bufferSize && status < TailStatus.ErrorThreshold)
                 {
                     var slept = 0;
-                    while (slept < BlobCheckChangedInterval && (!_resetTail))
+                    while (slept < BlobCheckChangedInterval && (!_doReset))
                     {
                         Thread.Sleep(SleepInterval);
                         slept += SleepInterval;
@@ -257,27 +257,58 @@ namespace BlobTail
 
             } while (true);
         }
-
-        private void StopButton_Click(object sender, EventArgs e)
+        private void TailWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            try
+            if (
+                e.ProgressPercentage == TailStatus.TextRead 
+                || e.ProgressPercentage == TailStatus.TextReadToEnd
+                )
             {
-                //_tailWorker.CancelAsync();
-                _currentBlob = null;
-                _resetTail = true;
-                EnumerateContainers();
-                ConnectButton.Enabled = true;
+                LogText.AppendText(e.UserState.ToString());
             }
-            catch (Exception)
+            else if (e.ProgressPercentage == TailStatus.AtEnd)
             {
-                // ignored
+                RefreshBlobsInCurrentContainer();
+            }
+            else if (e.ProgressPercentage > TailStatus.ErrorThreshold)
+            {
+                ResetTail();
+                MessageBox.Show(e.UserState?.ToString() ?? "Unknown error.");
+                EnumerateBlobs();
             }
         }
 
-        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        private void ConnectButton_Click(object sender, EventArgs e)
         {
-            Settings.Default["StorageAccountConnectionString"] = ConnectionString.Text;
-            Settings.Default.Save();
+            try
+            {
+                StorageAccount = CloudStorageAccount.Parse(ConnectionString.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            EnumerateContainers();
+        }
+
+
+        private void ContainerList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ResetTail();
+            EnumerateBlobs();
+        }
+
+        private void BlobList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StartTail();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            ResetTail();
+            EnumerateContainers();
         }
 
         private void FontBigger_Click(object sender, EventArgs e)
@@ -291,6 +322,21 @@ namespace BlobTail
             var font = LogText.Font;
             LogText.Font = new Font(font.FontFamily, font.Size - 2);
         }
-    }
 
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Settings.Default["StorageAccountConnectionString"] = ConnectionString.Text;
+            Settings.Default.Save();
+        }
+
+
+        private static IOrderedEnumerable<CloudBlob> GetBlobs(CloudBlobContainer container)
+        {
+            return container
+                .ListBlobs()
+                .OfType<CloudBlob>()
+                .OrderBy(b => b.Properties.LastModified);
+        }
+
+    }
 }
